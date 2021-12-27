44843b462a9d3dc502fd4a6c3e4cce26446e4503
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-27 11:42:52 +0200

    add fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,22 @@
-const btnStart = document.querySelector('[data-start]');
-const btnStop = document.querySelector('[data-stop]');
-
-const timer = {
-  intervalId: null,
-  start() {
-    const startTime = Date.now();
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      if (deltaTime) {
-        document.body.style.background = getRandomHexColor();
-        btnStart.setAttribute('disabled', true);
-      }
-    }, 1000);
-  },
-
-  stop() {
-    clearInterval(this.intervalId);
-    btnStart.removeAttribute('disabled');
-  },
-};
-
-btnStart.addEventListener('click', timer.start.bind());
-btnStop.addEventListener('click', timer.stop.bind());
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+timerId = null;
+btnStart.addEventListener("click", handlerColor);
+
+function handlerColor (event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true; 
+};
+
+btnStop.addEventListener("click", handlerStop);
+
+function handlerStop (event) {
+  if (timerId) {clearInterval(timerId);
+    btnStart.disabled = false}
+};
